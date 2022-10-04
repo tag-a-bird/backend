38ffe82065b2529f271529c72451f3b141738fd8
Can <>
2022-10-04 17:45:16 +0200

    http auth implemented

--- tag-a-bird-backend/tag_a_bird_backend/app.py
+++ tag-a-bird-backend/tag_a_bird_backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from dotenv import load_dotenv
+from os import getenv
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 load_dotenv()
 
@@ -13,7 +16,20 @@ db = SQLAlchemy(app)
 
 api = Api(app)
 
+auth = HTTPBasicAuth()
+
+users = {
+    str(getenv("USERNAME")): generate_password_hash(str(getenv("PASSWORD")))
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+
 @app.route("/")
+@auth.login_required
 def hello_world():
     return "<p>Hello, World!</p>"
 
