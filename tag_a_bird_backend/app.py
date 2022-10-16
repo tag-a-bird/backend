87a830fe1f18c851f88ef7a9e8c11f4864856592
kngjnssy <kingajanossy@gmail.com>
2022-10-16 12:27:25 +0200

    jwt authentication with Flask-JWT-Extended

--- tag_a_bird_backend/app.py
+++ tag_a_bird_backend/app.py
@@ -1,17 +1,24 @@
 from crypt import methods
-from flask import Flask, request, g
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from flask_httpauth import HTTPBasicAuth
-import json
-from .models import Base, User, Annotation
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 import datetime
 import uuid
+from dotenv import load_dotenv
+import json
+
+from .models import Base, User, Annotation
 
 app = Flask(__name__)
 
+load_dotenv()
+
 app.config.from_prefixed_env()
+
+jwt = JWTManager(app)
 
 engine = create_engine(app.config["DATABASE_URI"])
 
@@ -40,13 +47,35 @@ def get_users():
     users_list_dict = dict_helper(users_info)
     return users_list_dict
 
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/api/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity = username)
+    return jsonify(access_token = access_token)
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as = current_user), 200
+
 @auth.verify_password
 def verify_password(username, password):
     user = User.query.filter_by(username = username).first()
-    if not user or not user.verify_password(password):
-        return False
-    g.user = user
-    return True
+    if not user:
+        user = User.query.filter_by(username = username).first()
+        if not user or not user.verify_password(password):
+            return False
+    return user
 
 class Annotations(Resource):
     @auth.login_required
