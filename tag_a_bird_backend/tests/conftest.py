92351a8f8128e75946064811bbb73ef47b92fa21
kngjnssy <kingajanossy@gmail.com>
2022-10-24 21:19:53 +0200

    added test admin route and test for basic auth - might be unnecessary

---
+++ tag_a_bird_backend/tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from tag_a_bird_backend.app import app as flask_app
+
+@pytest.fixture
+def app():
+    yield flask_app
+@pytest.fixture
+def client(app):
+    return app.test_client()
