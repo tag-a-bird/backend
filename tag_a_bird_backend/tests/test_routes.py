45f2bf8c9e3fcff2645bb2b5d9ad77be99347cb6
kngjnssy <kingajanossy@gmail.com>
2022-11-08 15:48:54 +0100

    add tests

---
+++ tag_a_bird_backend/tests/test_routes.py
@@ -0,0 +1,22 @@
+def test_about_route(client):
+    assert client.get('/about').status_code == 200
+
+def test_signup_route(client):
+    assert client.get('/api/signup').status_code == 200
+    # assert client.post('/api/signup').status_code == 200
+
+def test_signin_route(client):
+    assert client.get('/api/signin').status_code == 200
+    # assert client.post('/api/signin').status_code == 200 
+
+def test_signout_route(client):
+    assert client.get('/api/signin').status_code == 200
+
+def test_admin_route(client):
+    assert client.get('/admin').status_code == 200
+
+def test_populate_db_route(client):
+    assert client.get('/admin/populate_db').status_code == 200
+
+def test_annotate_route(client):
+    assert client.get('/annotate').status_code == 200
