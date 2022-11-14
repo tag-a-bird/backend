9a46affe20d8a3f6e6d5f4c42bbf9e493dd315bc
kngjnssy <kingajanossy@gmail.com>
2022-11-14 13:09:03 +0100

    remove unused code

--- tag_a_bird_backend/app.py
+++ tag_a_bird_backend/app.py
@@ -1,18 +1,6 @@
-from crypt import methods
-from flask_restful import Resource
-import json
-from .models import User, Annotation, Base
-from . import create_app, auth, config
-from .db import db_session
+from . import create_app, config
     
 app = create_app(config.DevConfig)
-
-@auth.verify_password
-def verify_password(username, password):
-    user = User.query.filter_by(username = username).first()
-    if not user or not user.verify_password(password):
-        return False
-    return user
 
 if __name__ == '__main__':
     app.run()
