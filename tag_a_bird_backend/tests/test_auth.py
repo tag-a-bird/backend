d6267cebcc4f991b70a4160d76a9f34193b9d3e1
kngjnssy <kingajanossy@gmail.com>
2022-11-15 19:03:24 +0100

    write test first

--- tag_a_bird_backend/tests/test_auth.py
+++ tag_a_bird_backend/tests/test_auth.py
@@ -1,8 +1,20 @@
 import base64
+import datetime
+import uuid
 import pytest
 from flask import g, session
 from tag_a_bird_backend.models import User
 # from tag_a_bird_backend.tests.conftest import app
+
+def test_user_password_hashing():
+    user = User(
+                    id  = uuid.uuid4(),
+                    username = 'testuser',
+                    email = 'test@email.com',
+                    created_on = datetime.datetime.now()
+                    )
+    user.set_password('testpassword')
+    assert user.verify_password('testpassword')
 
 def test_request_with_logged_in_user(app):
     user = User.query.get('b40ecc45-3f4b-4be6-872f-d1c6cb2e7b49')
