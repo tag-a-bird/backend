d638698f40c491728ccf068f82811acd126a758d
kngjnssy <kingajanossy@gmail.com>
2022-11-16 13:58:34 +0100

    define user roles and automatically create admin

--- tag_a_bird_backend/app.py
+++ tag_a_bird_backend/app.py
@@ -1,6 +1,24 @@
+import datetime
+import uuid
 from . import create_app, config
+from .models import User, Role
+from .db import db_session, engine
+
     
 app = create_app(config.DevConfig)
+
+
+if not User.query.filter(User.email == 'admin@admin.com').first():
+    user = User(
+            id  = uuid.uuid4(),
+            username = 'admin',
+            email='admin@admin.com',
+            created_on = datetime.datetime.now()
+    )
+    user.set_password('password1')
+    user.roles.append(Role(name='Admin'))
+    db_session.add(user)
+    db_session.commit()
 
 if __name__ == '__main__':
     app.run()
