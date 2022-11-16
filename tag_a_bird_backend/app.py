eb55d8ffb1b7cc647814f4f04dab945f99558599
kngjnssy <kingajanossy@gmail.com>
2022-11-16 15:51:20 +0100

    create access required

--- tag_a_bird_backend/app.py
+++ tag_a_bird_backend/app.py
@@ -1,21 +1,34 @@
 import datetime
+from os import getenv
 import uuid
 from . import create_app, config
 from .models import User, Role
-from .db import db_session, engine
+from .db import db_session
+from functools import wraps
+from flask import session, jsonify
 
+def access_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_function(*args, **kwargs):
+            if session.get("role") == 'Admin' and role == 'Admin':
+                print("access: Admin")
+            else:
+                return jsonify({"msg": "Only the admin can access this page"}), 401
+            return fn(*args, **kwargs)
+        return decorated_function
+    return wrapper
     
 app = create_app(config.DevConfig)
 
-
-if not User.query.filter(User.email == 'admin@admin.com').first():
+if not User.query.filter(User.email == getenv('ADMIN_CREDENTIALS_EMAIL')).first():
     user = User(
             id  = uuid.uuid4(),
-            username = 'admin',
-            email='admin@admin.com',
+            username = "admin",
+            email= getenv('ADMIN_CREDENTIALS_EMAIL'),
             created_on = datetime.datetime.now()
     )
-    user.set_password('password1')
+    user.set_password(getenv('ADMIN_CREDENTIALS_PW'))
     user.roles.append(Role(name='Admin'))
     db_session.add(user)
     db_session.commit()
