b9555c1faaff1c61a59f37074c390c3f6c920ea9
kngjnssy <kingajanossy@gmail.com>
2022-12-05 21:14:01 +0100

    testing routes, also with Admin role in session

--- tag_a_bird_backend/tests/test_routes.py
+++ tag_a_bird_backend/tests/test_routes.py
@@ -1,23 +1,34 @@
-def test_about_route(client):
-    assert client.get('/about').status_code == 200
+def test_about_route(test_client):
+    assert test_client.get('/about').status_code == 200
+    # response = test_client.get("/about")
+    # assert b"Hello" in response.data
 
-def test_register_route(client):
-    assert client.get('/api/register').status_code == 200
-    # assert client.post('/api/register').status_code == 200
+def test_register_route(test_client):
+    assert test_client.get('/api/register').status_code == 200
+    # assert test_client.post('/api/register').status_code == 200
 
-def test_login_route(client):
-    assert client.get('/api/login').status_code == 200
-    # assert client.post('/api/login').status_code == 200 
+def test_login_route(test_client, new_user):
+    assert test_client.get('/api/login').status_code == 200
 
-def test_logout_route(client):
-    assert client.get('/api/login').status_code == 200
+    # assert test_client.post('/api/login').status_code == 200 
 
-# def test_admin_route(client):
-#     assert client.get('/admin').status_code == 200
+def test_logout_route(test_client):
+    response = test_client.get('/api/logout', follow_redirects=True)
+    assert len(response.history) == 1
+    assert response.request.path == "/api/login"
+    assert response.status_code == 200
 
-# def test_populate_db_route(client):
-#     assert client.get('/admin/populate_db').status_code == 200
+def test_admin_route(test_client):
+    assert test_client.get('/admin').status_code == 401
+    with test_client.session_transaction() as session:
+        session['role'] = "Admin"
+    assert test_client.get('/admin').status_code == 200
 
-def test_annotate_route(client):
-    pass
-    # assert client.get('/annotate').status_code == 200
+def test_populate_db_route(test_client):
+    assert test_client.get('/admin/populate_db').status_code == 401
+    with test_client.session_transaction() as session:
+        session['role'] = "Admin"
+    assert test_client.get('/admin/populate_db').status_code == 200
+
+def test_annotate_route(test_client):
+    assert test_client.get('/annotate').status_code == 200
