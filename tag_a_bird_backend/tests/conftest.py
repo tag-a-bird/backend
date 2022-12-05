c9ba9339e069919fc15eced7a9d9b248e0e37035
kngjnssy <kingajanossy@gmail.com>
2022-12-05 15:44:50 +0100

    clean up conftest, add test user fixture

--- tag_a_bird_backend/tests/conftest.py
+++ tag_a_bird_backend/tests/conftest.py
@@ -1,34 +1,33 @@
-import os
-import tempfile
 import pytest
+import datetime
+import uuid
 from tag_a_bird_backend import config, create_app
-from tag_a_bird_backend.db import Base, db_session
+from tag_a_bird_backend.models import User
 from flask_login import FlaskLoginClient
-
-# read in SQL for populating test data
-# with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
-#     _data_sql = f.read().decode("utf8")
 
 @pytest.fixture
 def app():
     """Create and configure a new app instance for each test."""
-    # create a temporary file to isolate the database for each test
-    # db_fd, db_path = tempfile.mkstemp()
-    # create the app with common test config
+
     app = create_app(config.TestConfig)
     app.test_client_class = FlaskLoginClient
 
-    # create the database and load test data
-
     yield app
 
-    # close and remove the temporary database
-
 @pytest.fixture
-def client(app):
+def test_client(app):
     yield app.test_client()
 
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
 
+@pytest.fixture(scope='module')
+def new_user():
+    user = User(
+                    id  = uuid.uuid4(),
+                    username = 'testuser',
+                    email = 'test@email.com',
+                    created_on = datetime.datetime.now()
+                    )
+    return user
