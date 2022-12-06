f7587bc885a5a5902a8d87b7470ef114a4419ff6
kngjnssy <kingajanossy@gmail.com>
2022-12-06 14:34:49 +0100

    cleanup fixtures, add test + data for testing annotation-record-user relation

--- tag_a_bird_backend/tests/conftest.py
+++ tag_a_bird_backend/tests/conftest.py
@@ -2,15 +2,14 @@ import pytest
 import datetime
 import uuid
 from tag_a_bird_backend import config, create_app
-from tag_a_bird_backend.models import User, Annotation, QueryConfig
+from tag_a_bird_backend.models import User, Annotation, QueryConfig, Record
 from flask_login import FlaskLoginClient
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from tag_a_bird_backend.db import Base
 
 @pytest.fixture
 def app():
-    """Create and configure a new app instance for each test."""
     app = create_app(config.TestConfig)
     app.test_client_class = FlaskLoginClient
 
@@ -24,7 +23,7 @@ def test_client(app):
 def runner(app):
     return app.test_cli_runner()
 
-@pytest.fixture()
+@pytest.fixture
 def new_user():
     user = User(
                 id  = uuid.uuid4(),
@@ -35,7 +34,7 @@ def new_user():
     user.set_password('testpassword')
     return user
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def setup_test_database(app):
     engine = create_engine(app.config['DATABASE_URI'])
     Base.metadata.create_all(engine)
@@ -44,7 +43,7 @@ def setup_test_database(app):
     yield session
     session.close()
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def test_data(setup_test_database):
     test_session = setup_test_database
     test_session.add_all([
@@ -56,4 +55,64 @@ def test_data(setup_test_database):
     test_session.commit()
     yield test_session
     test_session.query(QueryConfig).delete()
+    test_session.commit()
+
+@pytest.fixture
+def test_annotation(setup_test_database):
+    test_session = setup_test_database
+    test_session.add(
+        User(
+            id  = '2e3317a6-920c-42b9-a3c3-797422ac0b41',
+            username = 'fakeuser',
+            email = 'fake@email.com',
+            salt = b'fakesalt',
+            password_hash = b'fakehash',
+            created_on = '2022-12-06 12:34:36.019047'
+        ))
+    test_session.add(
+        Record(
+            id = 9811488,
+            created_at = '2022-12-06 12:34:36.019047',
+            audio_url = 'placeholder',
+            photo_url = 'placeholder',
+            comment = 'placeholder',
+            country = 'placeholder',
+            city = 'placeholder',
+            habitat = 'placeholder',
+            species = 'placeholder',
+            weather = 'placeholder',
+            date = '2022-12-06 12:34:36.019047',
+            device_os = 'placeholder',
+            is_holiday = False,
+            human_noise = True,
+            device_model = 'placeholder',
+            habitat_other = 'placeholder',
+            species_other = 'placeholder',
+            day_of_the_week = 'placeholder',
+            human_noise_type = 'placeholder',
+            location_private = True,
+            location_accuracy = 'placeholder',
+            dawn_chorus_import_id = 'placeholder',
+            human_noise_intensity = 'placeholder',
+            organization_membership = 'placeholder',
+            advanced_audio_equipment = 'placeholder',
+            status = 'placeholder'
+        ))
+    test_session.add(
+        Annotation(
+            id = 3,
+            recording_id = 9811488,
+            user_id = "2e3317a6-920c-42b9-a3c3-797422ac0b41",
+            start_time = 10,
+            end_time = 20,
+            label = "{Mönchsgrasmücke,Haussperling}",
+            status = "{}"
+        ))
+    test_session.commit()
+    yield test_session
+    test_session.query(Annotation).delete()
+    test_session.commit()
+    test_session.query(Record).delete()
+    test_session.commit()
+    test_session.query(User).delete()
     test_session.commit()
--- tag_a_bird_backend/tests/test_models.py
+++ tag_a_bird_backend/tests/test_models.py
@@ -1,4 +1,4 @@
-from tag_a_bird_backend.models import QueryConfig
+from tag_a_bird_backend.models import QueryConfig, User, Record, Annotation
 
 def test_new_user(new_user):
     """
@@ -9,12 +9,15 @@ def test_new_user(new_user):
     assert new_user.verify_password('testpassword')
     assert new_user.email == 'test@email.com'
 
-def test_query_config(test_client, test_data):
+def test_query_config(test_data):
     test_session = test_data
     assert test_session.query(QueryConfig).filter_by(parameter='country').first().value == "Hungary"
     assert test_session.query(QueryConfig).filter_by(parameter='with_annotation').first().value == "False"
     assert test_session.query(QueryConfig).filter_by(parameter='in_status').first().value == ""
     assert test_session.query(QueryConfig).filter_by(parameter='not_in_status').first().value == ""
 
-# def test_new_annotation(new_annotation):
-#     assert new_annotation.id == 12345
+def test_new_annotation(test_annotation):
+    test_session = test_annotation
+    assert test_session.query(User).filter_by(username='fakeuser')
+    assert test_session.query(Record).filter_by(id=9811488)
+    assert test_session.query(Annotation).filter_by(recording_id=9811488)
