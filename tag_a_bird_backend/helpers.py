1fdb01f1e15dec42b814382ccc1b4c7298911917
kngjnssy <kingajanossy@gmail.com>
2023-01-09 12:46:14 +0100

    fixed some of the db population logic

--- tag_a_bird_backend/helpers.py
+++ tag_a_bird_backend/helpers.py
@@ -76,30 +76,38 @@ def load_new_records_only(limit, offset, country, db_session):
 
     db_last_record = db_session.query(Record).filter_by(country=country).order_by(Record.created_at.desc()).first()
     count = 0
+    checked = 0
     print(f'there are already some records from {country}')
     # print(f'last record\'s id is {db_last_record.id}')
     response = coreo_request(offset=offset, limit=limit, country=country)
-        
+    print(len(response["data"]["records"]))
     while len(response["data"]["records"]) == limit:
         try:
             for record in response["data"]["records"]:
-                if record["id"] == db_last_record.id:
-                    flash(f'There are no new records from {country}')
-                    print(f'There are no new records from {country}')
-                    return
-                else:
-                    print('Adding new records to the database...')
-                    new_record = Record.from_json(json=record["data"], id=record["id"])
-                    db_session.add(new_record)
-                    count += 1
+                # if record["id"] == db_last_record.id:
+                #     flash(f'There are no new records from {country}')
+                #     print(f'There are no new records from {country}')
+                #     return
+                # else:
+                    cheked_id = record["id"]
+                    existing_id = db_session.query(Record).filter_by(id=cheked_id).first()
+                    if not existing_id:
+                        print('Adding new records to the database...')
+                        new_record = Record.from_json(json=record["data"], id=record["id"])
+                        db_session.add(new_record)
+                        count += 1
+                        checked += 1
+                    else:
+                        checked += 1
+                        print('this already exist..')
         except Exception as e:
             db_session.rollback()
             count = 0
             print(e)
             return f"Error: {e}"
         db_session.commit()
-        print(f"Added {count} new records to the database")
-        offset += count
+        # print(f"Added {count} new records to the database")
+        offset += checked
         total_count += count
         count = 0
         response = coreo_request(offset=offset, limit=limit, country=country)
@@ -119,5 +127,5 @@ def populate_db_from_coreo(db_session, country: str) -> str:
         load_all_records(limit, offset, country, db_session)
         
     else:
-        load_new_records_only()
+        load_new_records_only(limit, offset, country, db_session)
             
