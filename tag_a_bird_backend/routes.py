d720841dab857950de5e76241f7f04dc470768ac
Can <>
2023-03-17 12:27:48 +0100

    relative imports rearranged for docker

--- tag_a_bird_backend/routes.py
+++ tag_a_bird_backend/routes.py
@@ -2,14 +2,14 @@ from os import getenv
 from flask import request, jsonify, render_template, flash, url_for, redirect, Blueprint, session
 from flask_login import login_user, login_required, logout_user, current_user
 #from .validate_email import check_email
-from backend.tag_a_bird_backend.helpers import populate_db_from_coreo
+from tag_a_bird_backend.helpers import populate_db_from_coreo
 import datetime
 import uuid
-from backend.tag_a_bird_backend.models import Role, User, QueryConfig, Record, Annotation
-from backend.tag_a_bird_backend import login_manager, limiter
-from backend.tag_a_bird_backend.db import db_session, func
-from backend.tag_a_bird_backend.static.species import most_possible_birds, other_possible_birds
-from backend.tag_a_bird_backend.static.flags import flags_list
+from tag_a_bird_backend.models import Role, User, QueryConfig, Record, Annotation
+from tag_a_bird_backend import login_manager, limiter
+from tag_a_bird_backend.db import db_session, func
+from tag_a_bird_backend.static.species import most_possible_birds, other_possible_birds
+from tag_a_bird_backend.static.flags import flags_list
 from functools import wraps
 import requests
 
