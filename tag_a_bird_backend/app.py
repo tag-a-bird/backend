eb774592c0d15ff0a48a7caac0918e172869f5d4
Can <can.turgay@code.berlin>
2024-07-24 12:27:26 +0200

    database and servers dockerized

--- tag_a_bird_backend/app.py
+++ tag_a_bird_backend/app.py
@@ -4,18 +4,26 @@ import uuid
 from .models import User, Role
 from .db import db_session
 from . import create_app, config
- 
+
 app = create_app()
 
-if not db_session.query(User.email == getenv('ADMIN_CREDENTIALS_EMAIL')).first():
+# Check if the Admin role exists
+admin_role = db_session.query(Role).filter_by(name='Admin').first()
+if not admin_role:
+    admin_role = Role(name='Admin')
+    db_session.add(admin_role)
+    db_session.commit()
+
+# Check if the admin user exists
+if not db_session.query(User).filter_by(email=getenv('ADMIN_CREDENTIALS_EMAIL')).first():
     user = User(
-            id  = uuid.uuid4(),
-            username = "admin",
-            email= getenv('ADMIN_CREDENTIALS_EMAIL'),
-            created_on = datetime.datetime.now()
+        id=uuid.uuid4(),
+        username="admin",
+        email=getenv('ADMIN_CREDENTIALS_EMAIL'),
+        created_on=datetime.datetime.now()
     )
     user.set_password(getenv('ADMIN_CREDENTIALS_PW'))
-    user.roles.append(Role(name='Admin'))
+    user.roles.append(admin_role)
     db_session.add(user)
     db_session.commit()
 
