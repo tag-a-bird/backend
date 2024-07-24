7b490274e396f299b1ba947481b917d9921fc529
Can <can.turgay@code.berlin>
2024-07-24 10:32:34 +0200

    fix: alembic

--- tag_a_bird_backend/__init__.py
+++ tag_a_bird_backend/__init__.py
@@ -3,7 +3,6 @@ from flask_toastr import Toastr
 from flask_login import LoginManager
 from .db import configure_engine, db_session, engine
 from .models import Base
-from alembic import config as alembic_config
 from . import config
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -19,20 +18,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config.DevConfig)
 
-    #ssl_context = ssl.create_default_context(cafile='/Users/can/coding_enviorment/tag-a-bird/backend/tag_a_bird_backend/ca-certificate.cer')
-    #ssl_context.verify_mode = ssl.CERT_REQUIRED
-
-    #connect_arg = {
-   #     'ssl': ssl_context
-    #}
-
     configure_engine(app.config['DATABASE_URI'])
-
-    #alembicArgs = ['revision', '--autogenerate', '-m', 'auto-generated migration']
-    #alembic_config.main(argv=alembicArgs)
-    #alembicArgs = ['upgrade', 'head']
-    #alembic_config.main(argv=alembicArgs)
-    
 
     from tag_a_bird_backend.routes import route_blueprint
     app.register_blueprint(route_blueprint)
--- tag_a_bird_backend/db.py
+++ tag_a_bird_backend/db.py
@@ -1,15 +1,20 @@
-from sqlalchemy import create_engine, func
-from sqlalchemy.orm import declarative_base, scoped_session, create_session, sessionmaker
+from sqlalchemy import create_engine, func, pool
+from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
 
 Base = declarative_base()
 engine = None
-sessionmaker = sessionmaker()
-db_session = scoped_session(sessionmaker)
+Session = sessionmaker()
+db_session = scoped_session(Session)
 
 def configure_engine(url):
-    global sessionmaker, engine, db_session
+    global engine, db_session, Session
 
-    engine = create_engine(url)
+    engine = create_engine(url, poolclass=pool.NullPool)
     db_session.remove()
-    sessionmaker.configure(bind=engine)
+    Session.configure(bind=engine)
+    db_session.configure(bind=engine)
     Base.metadata.create_all(engine)
+
+def get_engine():
+    global engine
+    return engine
