563d8b66a0836cf707db55b8dcb4328838a68a60
Can <can.turgay@code.berlin>
2024-07-27 12:45:02 +0200

    debug

--- tag_a_bird_backend/helpers.py
+++ tag_a_bird_backend/helpers.py
@@ -32,12 +32,8 @@ def populate_db_from_coreo(db_session, country: str) -> str:
         }}"""
 
         request_body = {"query": query}
-        print('Request body:', request_body)
         try:
             response = requests.post(api_url, headers=request_header, json=request_body)
-            print('Request made:', request_body)
-            print('Status code:', response.status_code)
-            print('Response:', response.text)
             response.raise_for_status()
             return response.json()
         except requests.RequestException as e:
@@ -50,12 +46,9 @@ def populate_db_from_coreo(db_session, country: str) -> str:
         if response and "data" in response and "records" in response["data"]:
             count = 0
             records = response["data"]["records"]
-            print(f"Records received: {records}")
             if not records:
-                return "No records found or API request failed."
-
+                return "No records found or API request failed. {response}"
             for record in records:
-                print(f"Processing record: {record}")
                 if not db_session.query(Record).filter_by(id=record["id"]).first():
                     new_record = Record.from_json(json=record["data"], id=record["id"])
                     db_session.add(new_record)
