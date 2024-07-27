88f118410756fdd457b27064047da1c16c0af2c0
Can <can.turgay@code.berlin>
2024-07-27 14:54:43 +0200

    get post funcs seperated

--- tag_a_bird_backend/blueprints/admin/routes.py
+++ tag_a_bird_backend/blueprints/admin/routes.py
@@ -7,6 +7,14 @@ from ...helpers import populate_db_from_coreo
 from flask import jsonify
 
 admin_bp = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from functools import wraps
+from ...models import Role, QueryConfig
+from ...db import db_session
+from ...helpers import populate_db_from_coreo
+
+admin_bp = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
 def admin_access_required():
     def wrapper(fn):
@@ -20,52 +28,58 @@ def admin_access_required():
         return decorated_function
     return wrapper
 
-@admin_bp.route('/admin/populate_db', methods=["GET", "POST"])
+@admin_bp.route('/admin/populate_db', methods=["GET"])
 @login_required
 @admin_access_required()
-def populate_db():
-    if request.method == "GET":
-        return render_template('admin/populate_db.html')
-    elif request.method == "POST":
-        try:
-            country = request.form['country']
-            result = populate_db_from_coreo(db_session=db_session, country=country)
-            flash(result)
-        except Exception as e:
-            flash('Error: ' + str(e))
-            print(e)
-        return render_template('admin/populate_db.html')
+def populate_db_get():
+    return render_template('admin/populate_db.html')
 
-@admin_bp.route('/admin', methods=['GET', 'POST'])
+@admin_bp.route('/admin/populate_db', methods=["POST"])
 @login_required
 @admin_access_required()
-def set_parameters():
-    if request.method == 'GET':
-        if db_session.query(QueryConfig).first() is None:
-            country = QueryConfig(parameter='country', value='')
-            db_session.add(country)
-            with_annotation = QueryConfig(parameter='with_annotation', value='False')
-            db_session.add(with_annotation)
-            in_status = QueryConfig(parameter='in_status', value='')
-            db_session.add(in_status)
-            not_in_status = QueryConfig(parameter='not_in_status', value='')
-            db_session.add(not_in_status)
-            db_session.commit()
+def populate_db_post():
+    try:
+        country = request.form['country']
+        result = populate_db_from_coreo(db_session=db_session, country=country)
+        flash(result)
+    except Exception as e:
+        flash('Error: ' + str(e))
+        print(e)
+    return render_template('admin/populate_db.html')
+
+@admin_bp.route('/admin', methods=['GET'])
+@login_required
+@admin_access_required()
+def set_parameters_get():
+    if db_session.query(QueryConfig).first() is None:
+        country = QueryConfig(parameter='country', value='')
+        db_session.add(country)
+        with_annotation = QueryConfig(parameter='with_annotation', value='False')
+        db_session.add(with_annotation)
+        in_status = QueryConfig(parameter='in_status', value='')
+        db_session.add(in_status)
+        not_in_status = QueryConfig(parameter='not_in_status', value='')
+        db_session.add(not_in_status)
+        db_session.commit()
+    return render_template('/admin/admin.html', country=db_session.query(QueryConfig).filter_by(parameter='country').first().value, with_annotation=db_session.query(QueryConfig).filter_by(parameter='with_annotation').first().value, in_status=db_session.query(QueryConfig).filter_by(parameter='in_status').first().value, not_in_status=db_session.query(QueryConfig).filter_by(parameter='not_in_status').first().value)
+
+@admin_bp.route('/admin', methods=['POST'])
+@login_required
+@admin_access_required()
+def set_parameters_post():
+    try:
+        country = QueryConfig(parameter='country', value=request.form['country'])
+        db_session.merge(country)
+        with_annotation = QueryConfig(parameter='with_annotation', value=request.form['with_annotation'])
+        db_session.merge(with_annotation)
+        in_status = QueryConfig(parameter='in_status', value=request.form['in_status'])
+        db_session.merge(in_status)
+        not_in_status = QueryConfig(parameter='not_in_status', value=request.form['not_in_status'])
+        db_session.merge(not_in_status)
+        db_session.commit()
+        flash("Parameters successfully set.")
         return render_template('/admin/admin.html', country=db_session.query(QueryConfig).filter_by(parameter='country').first().value, with_annotation=db_session.query(QueryConfig).filter_by(parameter='with_annotation').first().value, in_status=db_session.query(QueryConfig).filter_by(parameter='in_status').first().value, not_in_status=db_session.query(QueryConfig).filter_by(parameter='not_in_status').first().value)
-    elif request.method == 'POST':
-        try:
-            country = QueryConfig(parameter='country', value=request.form['country'])
-            db_session.merge(country)
-            with_annotation = QueryConfig(parameter='with_annotation', value=request.form['with_annotation'])
-            db_session.merge(with_annotation)
-            in_status = QueryConfig(parameter='in_status', value=request.form['in_status'])
-            db_session.merge(in_status)
-            not_in_status = QueryConfig(parameter='not_in_status', value=request.form['not_in_status'])
-            db_session.merge(not_in_status)
-            db_session.commit()
-            flash("Parameters successfully set.")
-            return render_template('/admin/admin.html', country=db_session.query(QueryConfig).filter_by(parameter='country').first().value, with_annotation=db_session.query(QueryConfig).filter_by(parameter='with_annotation').first().value, in_status=db_session.query(QueryConfig).filter_by(parameter='in_status').first().value, not_in_status=db_session.query(QueryConfig).filter_by(parameter='not_in_status').first().value)
-        except Exception as e:
-            db_session.rollback()
-            flash('Error: ' + str(e))
-            return render_template('/admin/admin.html', country=db_session.query(QueryConfig).filter_by(parameter='country').first().value, with_annotation=db_session.query(QueryConfig).filter_by(parameter='with_annotation').first().value, in_status=db_session.query(QueryConfig).filter_by(parameter='in_status').first().value, not_in_status=db_session.query(QueryConfig).filter_by(parameter='not_in_status').first().value)
+    except Exception as e:
+        db_session.rollback()
+        flash('Error: ' + str(e))
+        return render_template('/admin/admin.html', country=db_session.query(QueryConfig).filter_by(parameter='country').first().value, with_annotation=db_session.query(QueryConfig).filter_by(parameter='with_annotation').first().value, in_status=db_session.query(QueryConfig).filter_by(parameter='in_status').first().value, not_in_status=db_session.query(QueryConfig).filter_by(parameter='not_in_status').first().value)
--- tag_a_bird_backend/blueprints/auth/routes.py
+++ tag_a_bird_backend/blueprints/auth/routes.py
@@ -8,9 +8,15 @@ from os import getenv
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 
-@auth_bp.route('/api/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST' and len(request.form['password']) > 7:
+@auth_bp.route('/api/register', methods=['GET'])
+def register_get():
+    if current_user.is_authenticated:
+        return render_template('about.html')
+    return render_template('auth/register.html')
+
+@auth_bp.route('/api/register', methods=['POST'])
+def register_post():
+    if len(request.form['password']) > 7:
         if db_session.query(User).filter(User.email == request.form['email']).first() is None:
             db_session.rollback()
             try:
@@ -32,36 +38,33 @@ def register():
         else:
             flash('Error: User already exists')
             return render_template('auth/register.html')
-    elif request.method == 'POST' and len(request.form['password']) < 8:
+    else:
         flash("The password must be at least 8 characters long!")
         return render_template('auth/register.html')
-    elif request.method == 'GET':
-        if current_user.is_authenticated:
-            return render_template('about.html')
-        return render_template('auth/register.html')
 
-@auth_bp.route('/api/login', methods=["POST", "GET"])
+@auth_bp.route('/api/login', methods=['GET'])
+def login_get():
+    if current_user.is_authenticated:
+        return render_template('about.html')
+    return render_template('auth/login.html')
+
+@auth_bp.route('/api/login', methods=['POST'])
 @limiter.limit("5 per hour", deduct_when=lambda response: response.status_code != 200)
-def login():
-    if request.method == 'POST':
-        try:
-            email = request.form['email']
-            password = request.form['password']
-            user = db_session.query(User).filter_by(email=email).first()
-            if user.email == getenv("ADMIN_CREDENTIALS_EMAIL"):
-                r = db_session.query(Role).get(1)
-                session['role'] = r.name
-            if not user or not user.verify_password(password):
-                return jsonify({"msg": "Bad email or password"}), 401
-            login_user(user)
-            return render_template('about.html')
-        except Exception as e:
-            print(e)
+def login_post():
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        user = db_session.query(User).filter_by(email=email).first()
+        if user.email == getenv("ADMIN_CREDENTIALS_EMAIL"):
+            r = db_session.query(Role).get(1)
+            session['role'] = r.name
+        if not user or not user.verify_password(password):
             return jsonify({"msg": "Bad email or password"}), 401
-    elif request.method == 'GET':
-        if current_user.is_authenticated:
-            return render_template('about.html')
-        return render_template('auth/login.html')
+        login_user(user)
+        return render_template('about.html')
+    except Exception as e:
+        print(e)
+        return jsonify({"msg": "Bad email or password"}), 401
 
 @auth_bp.route('/api/logout')
 def logout():
